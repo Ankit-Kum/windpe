6b0c97b5c66ca859b616a96515d8853217d63307
Goyal <Goyal@gmail.com>
2023-09-15 02:17:02 -0300

    modified the code

--- winepr.py
+++ winepr.py
@@ -1,4 +1,5 @@
-print("this if for winf qulity pred model")
+print("this if for wine qulity pred model")
+print("adding the ref of delhi team")
 echo "hi wirte the name of wine"
 read name
 print("Name of the wine is $name")
